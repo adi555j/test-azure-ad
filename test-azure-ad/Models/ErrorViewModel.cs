74201ee79282f174326b7d252cd26207f6211e54
Aditya Pushkar <apushkar@microsoft.com>
2021-03-16 17:50:57 -0400

    Add project files.

---
+++ test-azure-ad/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test_azure_ad.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
